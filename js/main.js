d6f24714cf5b1f994e0e896716fe19dc90ef1a51
gaaiiinnnn <g3gan93@gmail.com>
2022-06-24 10:45:57 +0900

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$('.allBtn').click(function(){
+    $('nav').animate({left:0});
+});
+$('nav button').click(function(){
+    $('nav').animate({left:'-100%'});
+});
+
+$(window).resize(function(){
+    if($('nav').removeAttr('style'));
+});
+//nav에 스크립트가 적용 된게 있으면 화면이 resize될 때 스크립트를 없애준다
